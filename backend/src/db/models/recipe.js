8e1c16e6312b4809b8f81be5008f50f543a02e0a
drossano <deanrossano+dev@gmail.com>
2025-10-13 18:05:23 -0400

    feat: add likes to recipe schema

--- backend/src/db/models/recipe.js
+++ backend/src/db/models/recipe.js
@@ -7,6 +7,7 @@ const recipeSchema = new Schema(
     ingredients: String,
     steps: String,
     image: String,
+    likes: [{ type: Schema.Types.ObjectId, ref: 'user' }],
   },
   { timestamps: true },
 )
