6b1f938c59434623d62c9091f91c203f976c7634
drossano <deanrossano+dev@gmail.com>
2025-10-14 17:07:41 -0400

    feat: show like and unlike buttons depending on if user likes post

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -1,17 +1,38 @@
+import { jwtDecode } from "jwt-decode";
 import { useAuth } from "../contexts/AuthContext.jsx";
 import PropTypes from "prop-types";
+import { User } from "./User.jsx";
 
 export function Likes({ likes }) {
   const [token] = useAuth();
+  const { sub } = jwtDecode(token);
   if (token) {
-    return (
-      <div>
-        <button>Likes</button> {likes.length}
-      </div>
-    );
-  } else {
-    return <div>Likes: {likes.length}</div>;
+    if (likes.includes(sub)) {
+      return (
+        <div>
+          <div>
+            <User id={sub} />
+          </div>
+          <button>Unlike</button> {likes.length}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div>
+            <User id={sub} />
+          </div>
+          <button>Like</button> {likes.length}
+        </div>
+      );
+    }
   }
+  return (
+    <div>
+      <div>{sub}</div>
+      <div>Likes: {likes.length}</div>
+    </div>
+  );
 }
 
 Likes.propTypes = {
