557417e97d4b8123ab7d988d0fd1af38e29761a8
drossano <deanrossano+dev@gmail.com>
2025-10-14 12:31:59 -0400

    feat: display like button only if logged in

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -1,7 +1,18 @@
+import { useAuth } from "../contexts/AuthContext.jsx";
 import PropTypes from "prop-types";
 
 export function Likes({ likes }) {
-  return <div>Likes: {likes.length}</div>;
+  const [token] = useAuth();
+  if (token) {
+    return (
+      <div>
+        <button>Likes</button>
+        {likes.length}
+      </div>
+    );
+  } else {
+    return <div>Likes: {likes.length}</div>;
+  }
 }
 
 Likes.propTypes = {
