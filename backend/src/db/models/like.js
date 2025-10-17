5944202f29da58592f47d81c4c35f777d0328a97
drossano <deanrossano+dev@gmail.com>
2025-10-16 23:14:26 -0400

    feat: prevent user from liking recipe multiple times

--- backend/src/db/models/like.js
+++ backend/src/db/models/like.js
@@ -1,13 +1,23 @@
 import mongoose, { Schema } from 'mongoose'
 
-const likeSchema = new Schema(
+const likeSchema = new mongoose.Schema(
   {
-    recipe: { type: Schema.Types.ObjectId, ref: 'recipe', required: true },
-    user: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+    recipe: {
+      type: Schema.Types.ObjectId,
+      ref: 'recipe',
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
     session: { type: String, required: true },
     date: { type: Date, required: true },
   },
   { timestamps: true },
 )
+
+likeSchema.index({ recipe: 1, user: 1 }, { unique: true })
 
 export const Like = mongoose.model('like', likeSchema)
