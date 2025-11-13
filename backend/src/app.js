2aa7c5f03c2c9fa3c22b6ea5f92195d883461504
drossano <deanrossano+dev@gmail.com>
2025-11-13 12:19:12 -0500

    feat: set up socket io backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,8 @@ import { ApolloServer } from '@apollo/server'
 import { expressMiddleware } from '@apollo/server/express4'
 import { typeDefs, resolvers } from './graphql/index.js'
 import { optionalAuth } from './middleware/jwt.js'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 
 const app = express()
 app.use(cors())
@@ -20,7 +22,21 @@ app.get('/', (req, res) => {
   res.send('Hello from Express!')
 })
 
-export { app }
+const server = createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log('user connected:', socket.id)
+  socket.on('disconnect', () => {
+    console.log('user disconnected:', socket.id)
+  })
+})
+export { server as app }
 
 const apolloServer = new ApolloServer({
   typeDefs,
